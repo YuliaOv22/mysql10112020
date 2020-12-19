695914abc231cc894f070a19835c61c1e9013f97
YuliaOv <ovvera12@mail.ru>
2020-12-19 17:53:43 +0300

    Homework files

---
+++ NoSQL_Tasks_1_3.sql
@@ -0,0 +1,56 @@
+﻿-- Практическое задание по теме “NoSQL”
+
+-- 1. В базе данных Redis подберите коллекцию для подсчета посещений с определенных IP-адресов.
+
+HSET visits '192.168.1.2' 1
+HINCRBY visits '192.168.1.2' 1
+HGET visits '192.168.1.2'
+HGETALL visits
+
+HSET visits '192.168.1.7' 1
+HINCRBY visits '192.168.1.7' 1
+HGET visits '192.168.1.7'
+HGETALL visits
+
+
+-- 2. При помощи базы данных Redis решите задачу поиска имени пользователя по электронному адресу и наоборот, 
+-- поиск электронного адреса пользователя по его имени.
+
+HSET user 'Vera Smith' 'vera@gmail.com'
+HSET user 'John Cage' 'johnc@gmail.com'
+HGET user 'Vera Smith'
+
+HSET email 'vera@gmail.com' 'Vera Smith'
+HSET email 'em89@yandex.ru' 'Emily Strong'
+HGET email 'em89@yandex.ru'
+
+ 
+-- 3. Организуйте хранение категорий и товарных позиций учебной базы данных shop в СУБД MongoDB.
+
+db.createCollection('catalogs')
+db.createCollection('products')
+
+db.catalogs.insert({name: 'Процессоры'})
+db.catalogs.insert({name: 'Мат.платы'})
+
+db.catalogs.find()
+
+db.products.insert({
+    name: 'Intel Core i3-8100',
+    description: 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.',
+    price: 7890.00,
+    catalogs: 'Процессоры'});
+
+db.products.insert({
+    name: 'Intel Core i5-7400',
+    description: 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.',
+    price: 12700.00,
+    catalogs: 'Процессоры'});
+
+db.products.insert({
+    name: 'ASUS ROG MAXIMUS X HERO',
+    description: 'Материнская плата ASUS ROG MAXIMUS X HERO, Z370, Socket 1151-V2, DDR4, ATX',
+    price: 19310.00,
+    catalogs: 'Мат.платы'});
+
+db.products.find()
---
+++ Optimization_Tasks_1_2.sql
@@ -0,0 +1,76 @@
+﻿-- Практическое задание по теме “Оптимизация запросов”
+
+-- 1. Создайте таблицу logs типа Archive. 
+-- Пусть при каждом создании записи в таблицах users, catalogs и products в таблицу logs помещается время и дата создания записи, 
+-- название таблицы, идентификатор первичного ключа и содержимое поля name.
+
+-- Создаем таблицу logs типа Archive
+DROP TABLE IF EXISTS logs;
+CREATE TABLE logs (
+  created_at DATETIME COMMENT 'Время и дата создания записи',
+  table_name VARCHAR(255) COMMENT 'Название таблицы',
+  table_id INT UNSIGNED COMMENT 'Идентификатор первичного ключа таблицы',
+  name_text VARCHAR(255) COMMENT 'Содержимое поля name таблицы'
+) COMMENT = 'Архив записей таблиц users, catalogs и products' ENGINE=Archive;
+
+-- Создаем триггер для вставки записей из таблицы users
+DROP TRIGGER IF EXISTS users_insert;
+DELIMITER //
+CREATE TRIGGER users_insert AFTER INSERT ON users
+FOR EACH ROW
+BEGIN
+	INSERT INTO logs SELECT created_at, 'users', id, name FROM users WHERE id = NEW.id;
+END//
+
+-- Создаем триггер для вставки записей из таблицы catalogs
+DROP TRIGGER IF EXISTS catalogs_insert;
+DELIMITER //
+CREATE TRIGGER catalogs_insert AFTER INSERT ON catalogs
+FOR EACH ROW
+BEGIN
+	INSERT INTO logs SELECT NOW(), 'catalogs', id, name FROM catalogs WHERE id = NEW.id;
+END//
+
+-- Создаем триггер для вставки записей из таблицы products
+DROP TRIGGER IF EXISTS products_insert;
+DELIMITER //
+CREATE TRIGGER products_insert AFTER INSERT ON products
+FOR EACH ROW
+BEGIN
+	INSERT INTO logs SELECT created_at, 'products', id, name FROM products WHERE id = NEW.id;
+END//
+
+-- Проверочные вставки
+INSERT INTO users (name, birthday_at) VALUES
+  ('Геннадий', '1990-10-05');
+ 
+INSERT INTO catalogs VALUES
+  (NULL, 'Процессоры');
+ 
+INSERT INTO products
+  (name, description, price, catalog_id)
+VALUES
+  ('Intel Core i3-8100', 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 7890.00, 1);
+
+SELECT * FROM logs;
+
+
+-- 2. (по желанию) Создайте SQL-запрос, который помещает в таблицу users миллион записей.
+
+-- Создаем процедуру для вставки записей
+DROP PROCEDURE IF EXISTS users_insert;
+DELIMITER //
+CREATE PROCEDURE users_insert (IN value INT)
+BEGIN
+	DECLARE i INT DEFAULT 1;
+    WHILE i <= value DO
+	  INSERT INTO users (id) VALUES (NULL);
+      SET i = i + 1;
+    END WHILE;
+END//
+
+-- Вызываем процедуру на вставку миллиона записей
+CALL users_insert(1000000);
+
+SELECT * FROM users ORDER BY id DESC;
+
